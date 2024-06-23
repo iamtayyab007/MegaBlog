a6a14ab42e26fa43375e13b6e6f2272fe22f7595
iamtayyab007 <tayyabm336@gmail.com>
2024-06-23 16:24:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <h1>Welcome to the Blog in Appwrite</h1>
+    </>
+  );
+}
+
+export default App;
